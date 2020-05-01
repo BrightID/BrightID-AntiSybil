687be3be830b3ee727eef039aa5f0a027c9741e6
Siftal <m.khanmohamadzadeh@gmail.com>
2020-05-01 17:59:58 +0900

    move attackers_in_groups to attacks

--- anti_sybil/tests/attacks/attackers_in_groups.py
+++ anti_sybil/tests/attacks/attackers_in_groups.py
@@ -1,5 +1,5 @@
-from anti_sybil import algorithms
 from anti_sybil.graphs.node import Node
+from anti_sybil import algorithms
 from anti_sybil.utils import *
 
 OUTPUT_FOLDER = './outputs/attackers_in_groups/'
@@ -171,6 +171,7 @@ def add_sybils(graph, sybil_edges, group):
         edges.append((nodes_dic[edge[0]], nodes_dic[edge[1]]))
     graph.add_edges_from(edges)
 
+
 graph = graphs.generators.brightid_backup.generate({
     'file_path': os.path.join(OUTPUT_FOLDER, 'temp')
 })
@@ -178,59 +179,61 @@ add_sybils(graph, sybil_edges1, 'sybil1')
 add_sybils(graph, sybil_edges2, 'sybil2')
 add_sybils(graph, sybil_edges3, 'sybil3')
 add_sybils(graph, sybil_edges4, 'sybil4')
-# add_sybils(graph, sybil_edges5, 'sybil5')
-# add_sybils(graph, sybil_edges6, 'sybil6')
-# add_sybils(graph, sybil_edges7, 'sybil7')
-# add_sybils(graph, sybil_edges8, 'sybil8')
-# add_sybils(graph, sybil_edges9, 'sybil9')
-# add_sybils(graph, sybil_edges10, 'sybil10')
+add_sybils(graph, sybil_edges5, 'sybil5')
+add_sybils(graph, sybil_edges6, 'sybil6')
+add_sybils(graph, sybil_edges7, 'sybil7')
+add_sybils(graph, sybil_edges8, 'sybil8')
+add_sybils(graph, sybil_edges9, 'sybil9')
+add_sybils(graph, sybil_edges10, 'sybil10')
 
 outputs = []
 
-# ranker = algorithms.SybilRank(graph, algorithm_options)
-# ranker.rank()
-# outputs.append(generate_output(graph, 'SybilRank'))
-# draw_graph(graph, os.path.join(OUTPUT_FOLDER, 'SybilRank.html'))
-#
-# reset_ranks(graph)
+reset_ranks(graph)
+
+ranker = algorithms.SybilRank(graph, algorithm_options)
+ranker.rank()
+outputs.append(generate_output(graph, 'SybilRank'))
+draw_graph(graph, os.path.join(OUTPUT_FOLDER, 'SybilRank.html'))
+
+reset_ranks(graph)
 
 ranker = algorithms.SybilGroupRank(graph, algorithm_options)
 ranker.rank()
-
 outputs.append(generate_output(graph, 'SybilGroupRank'))
 draw_graph(graph, os.path.join(OUTPUT_FOLDER, 'SybilGroupRank.html'))
 
-# reset_ranks(graph)
+reset_ranks(graph)
 
-# ranker = algorithms.GroupSybilRank(graph, algorithm_options)
-# ranker.rank()
-# outputs.append(generate_output(graph, 'IntraGroupWeight'))
-# draw_graph(graph, os.path.join(OUTPUT_FOLDER, 'IntraGroupWeight.html'))
-#
-# reset_ranks(graph)
-# algorithm_options['weaken_under_min'] = True
-#
-# ranker = algorithms.SybilRank(graph, algorithm_options)
-# ranker.rank()
-# outputs.append(generate_output(graph, 'SR_weaken'))
-# draw_graph(graph, os.path.join(OUTPUT_FOLDER, 'SR_weaken.html'))
-#
-# reset_ranks(graph)
-#
-# ranker = algorithms.SybilGroupRank(graph, algorithm_options)
-# ranker.rank()
-# outputs.append(generate_output(graph, 'SGR_weaken'))
-# draw_graph(graph, os.path.join(OUTPUT_FOLDER, 'SGR_weaken.html'))
-#
-# reset_ranks(graph)
+ranker = algorithms.GroupSybilRank(graph, algorithm_options)
+ranker.rank()
+outputs.append(generate_output(graph, 'IntraGroupWeight'))
+draw_graph(graph, os.path.join(OUTPUT_FOLDER, 'IntraGroupWeight.html'))
 
-# ranker = algorithms.GroupSybilRank(graph, algorithm_options)
-# ranker.rank()
-# outputs.append(generate_output(graph, 'IGW_weaken'))
-# draw_graph(graph, os.path.join(OUTPUT_FOLDER, 'IGW_weaken.html'))
-#
-# reset_ranks(graph)
-#
+reset_ranks(graph)
+
+algorithm_options['weaken_under_min'] = True
+
+ranker = algorithms.SybilRank(graph, algorithm_options)
+ranker.rank()
+outputs.append(generate_output(graph, 'SR_weaken'))
+draw_graph(graph, os.path.join(OUTPUT_FOLDER, 'SR_weaken.html'))
+
+reset_ranks(graph)
+
+ranker = algorithms.SybilGroupRank(graph, algorithm_options)
+ranker.rank()
+outputs.append(generate_output(graph, 'SGR_weaken'))
+draw_graph(graph, os.path.join(OUTPUT_FOLDER, 'SGR_weaken.html'))
+
+reset_ranks(graph)
+
+ranker = algorithms.GroupSybilRank(graph, algorithm_options)
+ranker.rank()
+outputs.append(generate_output(graph, 'IGW_weaken'))
+draw_graph(graph, os.path.join(OUTPUT_FOLDER, 'IGW_weaken.html'))
+
+reset_ranks(graph)
+
 # ranker = algorithms.GroupMergingRank(graph, algorithm_options)
 # ranker.rank()
 # outputs.append(generate_output(graph, 'GroupMerge'))
