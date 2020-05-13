f32dd4601719566118a7cd8690e755c65554c138
Siftal <m.khanmohamadzadeh@gmail.com>
2020-05-14 03:11:03 +0900

    separate the manual attack from simple attacks

--- anti_sybil/tests/attacks/manual_attack.py
+++ anti_sybil/tests/attacks/manual_attack.py
@@ -1,10 +1,38 @@
-from anti_sybil.graphs.node import Node
+import os
 import random
+from anti_sybil.utils import *
+from manual_attack_config import *
+from anti_sybil import algorithms
+from anti_sybil.graphs.node import Node
+
+outputs = []
+algorithm_options = {
+    'accumulative': ACCUMULATIVE,
+    'nonlinear_distribution': NONLINEAR_DISTRIBUTION,
+    'group_edge_weight': GROUP_EDGE_WEIGHT,
+    'thresholds': THRESHOLDS
+}
+
+
+def generate_graph():
+    rar_addr = os.path.join(INPUT_FOLDER, 'brightid.tar.gz')
+    zip_addr = os.path.join(INPUT_FOLDER, 'brightid.zip')
+    if not os.path.exists(os.path.join(INPUT_FOLDER, 'brightid.zip')):
+        if not os.path.exists(INPUT_FOLDER):
+            os.makedirs(INPUT_FOLDER)
+        backup = requests.get(BACKUP_URL)
+        with open(rar_addr, 'wb') as f:
+            f.write(backup.content)
+        tar_to_zip(rar_addr, zip_addr)
+    json_graph = from_dump(zip_addr)
+    graph = from_json(json_graph)
+    return graph
 
 # use `h_ + integer` format to connect to the honests nodes
 # use `s_ + integer` format to connect to the seed nodes
 # use `id_ + Brightid` format to connect to the node by Brightid of the node
 # When the "top" variable set True, the `integer` part is the position in the list of the nodes sorted in descending order
+
 
 group1 = [
     ['id_xGUyVQLYV80pajm8QP-9cfHC7xri49V58k02kqTAiUI', 's12'],
@@ -32,16 +60,16 @@ group2 = [
 ]
 
 group3 = [
-    ['s_1', 's1'],
-    ['s_1', 's2'],
-    ['s_1', 's3'],
-    ['s_1', 's4'],
-    ['s_1', 's5'],
-    ['s_1', 's6'],
-    ['s_1', 's7'],
-    ['s_1', 's8'],
-    ['s_1', 's9'],
-    ['s_1', 's10'],
+    ['s_5', 's1'],
+    ['s_5', 's2'],
+    ['s_5', 's3'],
+    ['s_5', 's4'],
+    ['s_5', 's5'],
+    ['s_5', 's6'],
+    ['s_5', 's7'],
+    ['s_5', 's8'],
+    ['s_5', 's9'],
+    ['s_5', 's10'],
 ]
 
 group4 = [
@@ -93,9 +121,73 @@ def update_graph(graph, group, group_name, top=True):
     graph.add_edges_from(edges)
 
 
-def generate(graph):
+def attack(graph):
     update_graph(graph, group1, 'sybil1', top=True)
     update_graph(graph, group2, 'sybil2', top=True)
     update_graph(graph, group3, 'sybil3', top=True)
     update_graph(graph, group4, 'sybil4', top=True)
     return graph
+
+
+def tests(graph, description, file_name):
+    global outputs
+    if SYBIL_RANK:
+        reset_ranks(graph)
+        ranker = algorithms.SybilRank(graph, algorithm_options)
+        ranker.rank()
+        outputs.append(generate_output(
+            graph, 'SybilRank\n{}'.format(description)))
+        draw_graph(graph, os.path.join(
+            OUTPUT_FOLDER, 'SR_{}.html'.format(file_name)))
+
+    if SYBIL_GROUP_RANK:
+        reset_ranks(graph)
+        ranker = algorithms.SybilGroupRank(graph, algorithm_options)
+        ranker.rank()
+        outputs.append(generate_output(
+            graph, 'SybilGroupRank\n{}'.format(description)))
+        draw_graph(graph, os.path.join(
+            OUTPUT_FOLDER, 'SGR_{}.html'.format(file_name)))
+
+    if INTRA_GROUP_WEIGHT:
+        reset_ranks(graph)
+        ranker = algorithms.GroupSybilRank(graph, algorithm_options)
+        ranker.rank()
+        outputs.append(generate_output(
+            graph, 'IntraGroupWeight\n{}'.format(description)))
+        draw_graph(graph, os.path.join(
+            OUTPUT_FOLDER, 'IGW_{}.html'.format(file_name)))
+
+    if GROUP_MERGE:
+        reset_ranks(graph)
+        ranker = algorithms.GroupMergingRank(graph, algorithm_options)
+        ranker.rank()
+        outputs.append(generate_output(
+            graph, 'GroupMerge\n{}'.format(description)))
+        draw_graph(graph, os.path.join(
+            OUTPUT_FOLDER, 'GM_{}.html'.format(file_name)))
+
+    if WEIGHTED_SYBIL_RANK:
+        reset_ranks(graph)
+        ranker = algorithms.WeightedSybilRank(graph, algorithm_options)
+        ranker.rank()
+        outputs.append(generate_output(
+            graph, 'WeightedSybilRank\n{}'.format(description)))
+        draw_graph(graph, os.path.join(
+            OUTPUT_FOLDER, 'WSR_{}.html'.format(file_name)))
+
+
+def main():
+    # making the graph and ranking nodes
+    graph = generate_graph()
+    ranker = algorithms.SybilGroupRank(graph, algorithm_options)
+    ranker.rank()
+
+    graph = attack(graph)
+    tests(graph, 'manual attack', 'manual_attack')
+
+    write_output_file(outputs, os.path.join(OUTPUT_FOLDER, 'result.csv'))
+
+
+if __name__ == '__main__':
+    main()
--- anti_sybil/tests/attacks/run.py
+++ anti_sybil/tests/attacks/run.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import multiprocessing
+import requests
 import copy
 import time
 import os
 import collaborative_attacks as collaborative
-import manual_attack as manual
 import lone_attacks as lone
 from anti_sybil import algorithms
 from anti_sybil.utils import *
@@ -14,8 +14,8 @@ x_axis = []
 
 manager = multiprocessing.Manager()
 outputs = manager.list()
-charts = manager.dict({'SR': manager.dict(), 'SGR': manager.dict(
-), 'IGW': manager.dict(), 'GM': manager.dict(), 'WSR': manager.dict()})
+charts = manager.dict({'SR': manager.dict(), 'SGR1': manager.dict(), 'SGR2': manager.dict(
+), 'SGR': manager.dict(), 'IGW': manager.dict(), 'GM': manager.dict(), 'WSR': manager.dict()})
 
 algorithm_options = {
     'accumulative': ACCUMULATIVE,
@@ -23,6 +23,21 @@ algorithm_options = {
     'group_edge_weight': GROUP_EDGE_WEIGHT,
     'thresholds': THRESHOLDS
 }
+
+
+def generate_graph():
+    rar_addr = os.path.join(INPUT_FOLDER, 'brightid.tar.gz')
+    zip_addr = os.path.join(INPUT_FOLDER, 'brightid.zip')
+    if not os.path.exists(os.path.join(INPUT_FOLDER, 'brightid.zip')):
+        if not os.path.exists(INPUT_FOLDER):
+            os.makedirs(INPUT_FOLDER)
+        backup = requests.get(BACKUP_URL)
+        with open(rar_addr, 'wb') as f:
+            f.write(backup.content)
+        tar_to_zip(rar_addr, zip_addr)
+    json_graph = from_dump(zip_addr)
+    graph = from_json(json_graph)
+    return graph
 
 
 def tests(graph, description, file_name, outputs, charts):
@@ -34,7 +49,17 @@ def tests(graph, description, file_name, outputs, charts):
             graph, 'SybilRank\n{}'.format(description)))
         draw_graph(graph, os.path.join(
             OUTPUT_FOLDER, 'SR_{}.html'.format(file_name)))
-        charts['SR'][file_name] = successful_honests(graph)
+        charts['SR'][file_name] = successful_honests(graph, description, 'sybil rank')
+
+    if SYBIL_GROUP_RANK_V1:
+        reset_ranks(graph)
+        ranker = algorithms.V1SybilGroupRank(graph, algorithm_options)
+        ranker.rank()
+        outputs.append(generate_output(
+            graph, 'V1 SybilGroupRank\n{}'.format(description)))
+        draw_graph(graph, os.path.join(
+            OUTPUT_FOLDER, 'SGR1_{}.html'.format(file_name)))
+        charts['SGR1'][file_name] = successful_honests(graph, description, 'sybil group rank v1')
 
     if SYBIL_GROUP_RANK:
         reset_ranks(graph)
@@ -44,7 +69,7 @@ def tests(graph, description, file_name, outputs, charts):
             graph, 'SybilGroupRank\n{}'.format(description)))
         draw_graph(graph, os.path.join(
             OUTPUT_FOLDER, 'SGR_{}.html'.format(file_name)))
-        charts['SGR'][file_name] = successful_honests(graph)
+        charts['SGR'][file_name] = successful_honests(graph, description, 'sybil group rank')
 
     if INTRA_GROUP_WEIGHT:
         reset_ranks(graph)
@@ -54,7 +79,7 @@ def tests(graph, description, file_name, outputs, charts):
             graph, 'IntraGroupWeight\n{}'.format(description)))
         draw_graph(graph, os.path.join(
             OUTPUT_FOLDER, 'IGW_{}.html'.format(file_name)))
-        charts['IGW'][file_name] = successful_honests(graph)
+        charts['IGW'][file_name] = successful_honests(graph, description, 'intra group weight')
 
     if GROUP_MERGE:
         reset_ranks(graph)
@@ -64,7 +89,7 @@ def tests(graph, description, file_name, outputs, charts):
             graph, 'GroupMerge\n{}'.format(description)))
         draw_graph(graph, os.path.join(
             OUTPUT_FOLDER, 'GM_{}.html'.format(file_name)))
-        charts['GM'][file_name] = successful_honests(graph)
+        charts['GM'][file_name] = successful_honests(graph, description, 'group merge')
 
     if WEIGHTED_SYBIL_RANK:
         reset_ranks(graph)
@@ -74,20 +99,23 @@ def tests(graph, description, file_name, outputs, charts):
             graph, 'WeightedSybilRank\n{}'.format(description)))
         draw_graph(graph, os.path.join(
             OUTPUT_FOLDER, 'WSR_{}.html'.format(file_name)))
-        charts['WSR'][file_name] = successful_honests(graph)
+        charts['WSR'][file_name] = successful_honests(graph, description, 'weighted sybil rank')
 
 
-def successful_honests(graph):
+def successful_honests(graph, description, algorithm):
     honests = []
     sybils = []
     for node in graph.nodes:
         if node.node_type in ['Sybil', 'Non Bridge Sybil', 'Bridge Sybil']:
             sybils.append(node.rank)
-        if node.node_type in ['Seed', 'Honest'] and node.rank:  # ?Attacker
+        if node.node_type in ['Seed', 'Honest'] and node.rank > 0:
             honests.append(node.rank)
-    honests.sort()
     avg_sybils = sum(sybils) / len(sybils)
-    return (1 - (bisect(honests, avg_sybils) / len(honests))) * 100
+    verified = len([h for h in honests if h > avg_sybils])
+    percent = verified / len(honests) * 100
+    print('{}\nAlgorithm:\t{}'.format(description, algorithm))
+    print('No. honests:\t{}\nNo. sybils:\t{}\nNo. verified:\t{}\nPercent:\t{}\n\n'.format(len(honests), len(sybils), verified, percent))
+    return percent
 
 
 def plot():
@@ -99,7 +127,7 @@ def plot():
             x_axis,
             [charts[chart][p] for p in x_axis],
             'go--',
-            color='C{}'.format(i),
+            color='C{}'.format(i * 2),
             label='= {}'.format(chart),
         )
     ax.legend()
@@ -107,16 +135,14 @@ def plot():
         N_TARGETS, N_SYBILS, N_ATTACKERS, N_STITCHES, N_GROUPS))
     plt.xticks(rotation=90)
     plt.tight_layout()
-    plt.savefig(os.path.join(OUTPUT_FOLDER, 'algorithms.png'))
+    plt.savefig(os.path.join(OUTPUT_FOLDER, str(int(time.time())) + '.png'))
 
 
 def main():
     global outputs, charts
 
     # making the graph and ranking nodes
-    graph = graphs.generators.brightid_backup.generate({
-        'file_path': os.path.join(OUTPUT_FOLDER, 'temp')
-    })
+    graph = generate_graph()
     ranker = algorithms.SybilGroupRank(graph, algorithm_options)
     ranker.rank()
 
@@ -145,7 +171,7 @@ def main():
 
     if ONE_ATTACKER_GROUP_TARGET_ATTACK:
         _graph = lone.group_attack(copy.deepcopy(
-            graph), N_SYBILS, N_GROUPS, stitches=100)
+            graph), N_SYBILS, N_GROUPS, stitches=N_STITCHES)
         inputs.append([_graph, 'one attacker group target attack',
                        'one_attacker_group_target_attack', outputs, charts])
         x_axis.append('one_attacker_group_target_attack')
@@ -153,7 +179,7 @@ def main():
     if ONE_SEED_AS_ATTACKER:
         _graph = lone.collsion_attack(copy.deepcopy(
             graph), 'Seed', N_SYBILS, stitches=N_STITCHES)
-        inputs.append([_graph, 'one seed asattacker',
+        inputs.append([_graph, 'one seed as attacker',
                        'one_seed_as_attacker', outputs, charts])
         x_axis.append('one_seed_as_attacker')
 
@@ -188,21 +214,21 @@ def main():
 
     if ONE_GROUP_GROUP_TARGET_ATTACK:
         _graph = collaborative.group_attack(copy.deepcopy(
-            graph), N_ATTACKERS, N_TARGETS, N_SYBILS, N_GROUPS, stitches=100)
+            graph), N_ATTACKERS, N_TARGETS, N_SYBILS, N_GROUPS, stitches=N_STITCHES)
         inputs.append([_graph, 'one group group target attack',
                        'one_group_group_target_attack', outputs, charts])
         x_axis.append('one_group_group_target_attack')
 
     if ONE_GROUP_OF_SEEDS_AS_ATTACKER:
         _graph = collaborative.collsion_attack(
-            copy.deepcopy(graph), 'Seed', N_ATTACKERS, N_SYBILS, one_group=True, stitches=N_STITCHES)
+            copy.deepcopy(graph), 'Seed', N_UNFAITHFUL_SEEDS, N_SYBILS, one_group=True, stitches=N_STITCHES)
         inputs.append([_graph, 'one group of seeds as attacker',
                        'one_group_of_seeds_as_attacker', outputs, charts])
         x_axis.append('one_group_of_seeds_as_attacker')
 
     if ONE_GROUP_OF_HONESTS_AS_ATTACKER:
         _graph = collaborative.collsion_attack(
-            copy.deepcopy(graph), 'Honest', N_ATTACKERS, N_SYBILS, one_group=True, stitches=N_STITCHES)
+            copy.deepcopy(graph), 'Honest', N_UNFAITHFUL_HONESTS, N_SYBILS, one_group=True, stitches=N_STITCHES)
         inputs.append([_graph, 'one group of honests as attacker',
                        'one_group_of_honests_as_attacker', outputs, charts])
         x_axis.append('one_group_of_honests_as_attacker')
@@ -231,23 +257,17 @@ def main():
 
     if N_GROUPS_OF_SEEDS_AS_ATTACKER:
         _graph = collaborative.collsion_attack(
-            copy.deepcopy(graph), 'Seed', N_ATTACKERS, N_SYBILS, one_group=False, stitches=N_STITCHES)
+            copy.deepcopy(graph), 'Seed', N_UNFAITHFUL_SEEDS, N_SYBILS, one_group=False, stitches=N_STITCHES)
         inputs.append([_graph, 'n groups of seeds as attacker',
                        'n_groups_of_seeds_as_attacker', outputs, charts])
         x_axis.append('n_groups_of_seeds_as_attacker')
 
     if N_GROUPS_OF_HONESTS_AS_ATTACKER:
         _graph = collaborative.collsion_attack(
-            copy.deepcopy(graph), 'Honest', N_ATTACKERS, N_SYBILS, one_group=False, stitches=N_STITCHES)
+            copy.deepcopy(graph), 'Honest', N_UNFAITHFUL_HONESTS, N_SYBILS, one_group=False, stitches=N_STITCHES)
         inputs.append([_graph, 'n groups of honests as attacker',
                        'n_groups_of_honests_as_attacker', outputs, charts])
         x_axis.append('n_groups_of_honests_as_attacker')
-
-    if MANUAL_ATTACK:
-        _graph = manual.generate(copy.deepcopy(graph))
-        inputs.append([_graph, 'manual attack',
-                       'manual_attack', outputs, charts])
-        x_axis.append('manual_attack')
 
     with multiprocessing.Pool(processes=None) as pool:
         pool.starmap(tests, inputs)
