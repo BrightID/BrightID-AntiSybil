a94db26b562f4408c315b6340828e0bb26f64f37
Siftal <m.khanmohamadzadeh@gmail.com>
2020-05-18 14:28:21 +0900

    update compare test

--- anti_sybil/tests/compare_test.py
+++ anti_sybil/tests/compare_test.py
@@ -1,8 +1,9 @@
-import copy
 import os
+import copy
 from anti_sybil import algorithms
 from anti_sybil.utils import *
 
+# This script helps to compare the result of different anti-sybil algorithms by drawing a compare graph.
 
 OUTPUT_FOLDER = './outputs/compare_test/'
 
@@ -14,29 +15,26 @@ graph = load_brightid_graph({
     'file_path': os.path.abspath(os.path.join(OUTPUT_FOLDER, 'temp'))
 })
 
-# GroupSybilRank V01 with the stupid sybil border option
-border = stupid_sybil_border(graph)
+# SybilRank
 reset_ranks(graph)
-ranker = algorithms.V1GroupSybilRank(graph, {
-    'stupid_sybil_border': border
-})
+ranker = algorithms.SybilRank(graph)
 ranker.rank()
 graph1 = copy.deepcopy(ranker.graph)
 output1 = generate_output(
-    graph1, 'GroupSybilRank V01\n(stupid sybil border)')
+    graph1, 'SybilRank')
 draw_graph(ranker.graph, os.path.join(OUTPUT_FOLDER, 'graph_v01.html'))
 
 
-# GroupSybilRank with the stupid sybil border option
+# GroupSybilRank
 border = stupid_sybil_border(graph)
 reset_ranks(graph)
 ranker = algorithms.GroupSybilRank(graph, {
-    'cleaning': True
+    'stupid_sybil_border': border
 })
 ranker.rank()
 graph2 = copy.deepcopy(ranker.graph)
 output2 = generate_output(
-    graph2, 'GroupSybilRank\n(stupid sybil border)')
+    graph2, 'GroupSybilRank')
 draw_graph(ranker.graph, os.path.join(OUTPUT_FOLDER, 'graph_v02.html'))
 
 # Generate result
