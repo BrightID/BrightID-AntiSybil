740a501f4b33bdd1e031cc6640f60974fa7fad2c
Siftal <m.khanmohamadzadeh@gmail.com>
2020-08-19 18:54:39 +0430

    add landing probability algorithm

--- anti_sybil/algorithms/__init__.py
+++ anti_sybil/algorithms/__init__.py
@@ -5,3 +5,4 @@ from .sybil_rank import SybilRank
 from .group_sybil_rank import GroupSybilRank
 from .group_sybil_rank_v01 import GroupSybilRank as V1GroupSybilRank
 from .weighted_sybil_rank import WeightedSybilRank
+from .landing_probability import LandingProbability
