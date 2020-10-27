dbd78356c2865b477cb5aca5acd2ef94f3bf2f37
Siftal <m.khanmohamadzadeh@gmail.com>
2020-10-27 21:12:47 +0330

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="anti_sybil",
-    version="1.1.1",
+    version="1.2.0",
     author="Abram Symons",
     author_email="abram.symons@protonmail.com",
     description="Anti sybil package for BrightID",
